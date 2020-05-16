87e2d444c063548f86eb3c709c4631b56cae3acc
ecoble <elizabethjiamei@gmail.com>
2020-05-15 20:28:34 -0500

    add smooth scroll

--- js/top-button.js
+++ js/top-button.js
@@ -14,6 +14,9 @@ function scrollFunction() {
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth"
+  });
 }
